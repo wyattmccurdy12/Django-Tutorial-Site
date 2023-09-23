1cfaf09fdeb9ef36d8f452862aed416bfb5ca94e
Wyatt McCurdy <36129149+wyattmccurdy12@users.noreply.github.com>
2023-09-23 09:09:12 -0400

    Advance in tutorial - edit admin page

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -9,6 +10,12 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    @admin.display(
+            boolean = True,
+            ordering = "pub_date",
+            description="Published Recently"
+    )
 
     def was_published_recently(self):
         now = timezone.now()
